d2b89ce262eb4dce0b3dac439ab77215011fe150
jrgleason <jackiegleason@gmail.com>
2020-04-14 11:18:11 -0400

    Testing out movement through the interface

---
+++ robot/node/ui/movement/index.mjs
@@ -0,0 +1,22 @@
+import path from 'path';
+import {execSync} from 'child_process';
+
+import { SubRoute } from '../SubRoute.mjs';
+import { __dirname } from '../unclean.mjs';
+
+export class Movement extends SubRoute {
+  constructor() {
+    super();
+    this.setRoute('get', '/forward', this.onForwardGet.bind(this));
+  }
+  async onForwardGet(ctx) {
+    try{
+        const stdOut = execSync("python3 /home/jgleason/Code/pi-serial-example/robot/python/robot-hack/forward.py")
+        ctx.body = stdOut.toString('utf-8');
+    } catch(err){
+        // TODO: Remove error message
+        ctx.body = `There was an internal service error ${err}`;
+        ctx.response.status = 500;
+    }
+  }
+}
--- robot/node/ui/server.mjs
+++ robot/node/ui/server.mjs
@@ -4,14 +4,23 @@ import serve from 'koa-static';
 import {KoaSass} from '@jrg/koa-sass';
 
 import {__dirname} from './unclean.mjs';
-import {Main} from './main/Main.mjs';
+import {Main} from './main/index.mjs';
+import {Movement} from './movement/index.mjs';
+
+
+
+const main = new Main();
+const movement = new Movement();
 
 const app = new Koa();
-const main = new Main();
 const stylesApp = new Koa();
+
 // TODO: Move to index.mjs in styles folder
 const sass = new KoaSass({
   prefix: 'global',
+  loadPaths: [
+    "node_modules"
+  ]
 });
 
 stylesApp.use(sass.koaMiddleware);
@@ -19,6 +28,7 @@ stylesApp.use(serve('styles'));
 
 // app.use(mount('/ui', serve('./node_modules/@jrg/ubuntu-ui/dist')));
 app.use(mount('/', main.app))
+    .use(mount('/movement', movement.app))
     .use(mount('/styles', stylesApp))
     .use(mount('/material', serve(`${__dirname}/node_modules/@material`)))
     .listen(3001, ()=>{
