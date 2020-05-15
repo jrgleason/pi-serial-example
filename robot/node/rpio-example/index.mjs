5e4d770850cfe24be02eceeb4935f54e674e79ab
jrgleason <jackiegleason@gmail.com>
2020-05-14 22:00:07 -0400

    Trying to add the sensor

--- robot/node/rpio-example/index.mjs
+++ robot/node/rpio-example/index.mjs
@@ -5,12 +5,18 @@ const FORWARD_LEFT=16;
 const FORWARD_RIGHT=11;
 const BACKWARD_LEFT=13;
 const BACKWARD_RIGHT=15;
+const SENSOR=33;
 
-function blinkBlue(){
-    rpio.open(BLUE_LED, rpio.OUTPUT, rpio.High);
-    rpio.sleep(1);
+rpio.init({gpiomem: false});
+
+function blinkBlue(time){
+    console.log("Blinking");
+    rpio.open(BLUE_LED, rpio.OUTPUT, rpio.LOW);
+    rpio.write(BLUE_LED, rpio.HIGH);
+    rpio.sleep(time);
     rpio.close(BLUE_LED);
 }
+
 function goForward(time){
     rpio.open(FORWARD_LEFT, rpio.OUTPUT, rpio.HIGH);
     rpio.open(FORWARD_RIGHT, rpio.OUTPUT, rpio.HIGH);
@@ -20,8 +26,16 @@ function goForward(time){
 }
 
 (()=>{
+    rpio.open(SENSOR, rpio.PWM);
+    const int = setInterval(()=>{
+        console.log(`The sensor is ${rpio.read(SENSOR)}`)
+    }, 1000);
     goForward(2);
-    blinkBlue();
+    blinkBlue(3);
+    setTimeout(()=>{
+        console.log("Closing sensor");
+        rpio.close(SENSOR);
+    }, 50000)
 })();
 
 
