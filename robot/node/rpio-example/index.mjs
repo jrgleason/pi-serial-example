d41895616813c5e0fadf0e5692c3891a11090ceb
jrgleason <jackiegleason@gmail.com>
2020-05-14 21:24:22 -0400

    More rpio improvements

--- robot/node/rpio-example/index.mjs
+++ robot/node/rpio-example/index.mjs
@@ -1,16 +1,29 @@
 import rpio from "rpio";
 
-console.log("Running the RPIO robot example");
-rpio.open(16, rpio.OUTPUT, rpio.LOW);
+const BLUE_LED = 18;
+const FORWARD_LEFT=16;
+const FORWARD_RIGHT=11;
+const BACKWARD_LEFT=13;
+const BACKWARD_RIGHT=15;
 
-/*
- * https://github.com/jperkin/node-rpio
- */
-for (var i = 0; i < 5; i++) {
-    /* On for 1 second */
-    rpio.write(16, rpio.HIGH);
+function blinkBlue(){
+    rpio.open(BLUE_LED, rpio.OUTPUT, rpio.High);
     rpio.sleep(1);
-    /* Off for half a second (500ms) */
-    rpio.write(16, rpio.LOW);
-    rpio.msleep(500);
+    rpio.close(BLUE_LED);
 }
+function goForward(time){
+    rpio.open(FORWARD_LEFT, rpio.OUTPUT, rpio.HIGH);
+    rpio.open(FORWARD_RIGHT, rpio.OUTPUT, rpio.HIGH);
+    rpio.sleep(time);
+    rpio.close(FORWARD_LEFT);
+    rpio.close(FORWARD_LEFT);
+}
+
+(()=>{
+    goForward(2);
+    blinkBlue();
+})();
+
+
+
+
